765eaede5e56d9c9bb8d6434c4a4ac7bbce909a2
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-10-10 14:35:53 +0200

    Feat: Add new elements 'button' to play

--- index.js
+++ index.js
@@ -17,40 +17,58 @@ function getHumanChoice() {
   return choice;
 };
 
-function playGame () {
-  let total = 0;
-  let computerScore = 0;
-  let humanScore = 0;
+// function playGame () {
+//   let total = 0;
+//   let computerScore = 0;
+//   let humanScore = 0;
 
-  while (total < 5) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+//   while (total < 5) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-    total++
-  };
+//     playRound(humanSelection, computerSelection);
+//     total++
+//   };
 
-  function playRound(humanChoice, computerChoice) {
-    const humanChoiceConverted = humanChoice.toLowerCase();
-    const computerChoiceConverted = computerChoice.toLowerCase();
+//   function playRound(humanChoice, computerChoice) {
+//     const humanChoiceConverted = humanChoice.toLowerCase();
+//     const computerChoiceConverted = computerChoice.toLowerCase();
 
-    if (humanChoiceConverted === "rock" && computerChoiceConverted === "scissors"
-      || humanChoiceConverted === "paper" && computerChoiceConverted === "rock"
-      || humanChoiceConverted === "scissors" && computerChoiceConverted === "paper"
-    ) {
-      console.log(`You win ! ${humanChoiceConverted} beats ${computerChoiceConverted}`);
-      humanScore += 1;
-    } else if (humanChoiceConverted === "rock" && computerChoiceConverted === "paper"
-      || humanChoiceConverted === "paper" && computerChoiceConverted === "scissors"
-      || humanChoiceConverted === "scissors" && computerChoiceConverted === "rock"
-    ) {
-      console.log(`You lose... ${computerChoiceConverted} beats ${humanChoiceConverted}.`);
-      computerScore += 1;
-    } else {
-      console.log(`It's a tie. You choose ${humanChoiceConverted} and Computer also!`);
-    }
-    console.log(`Votre score: ${humanScore} - Computer's score: ${computerScore}`);
-  };
-}
+//     if (humanChoiceConverted === "rock" && computerChoiceConverted === "scissors"
+//       || humanChoiceConverted === "paper" && computerChoiceConverted === "rock"
+//       || humanChoiceConverted === "scissors" && computerChoiceConverted === "paper"
+//     ) {
+//       console.log(`You win ! ${humanChoiceConverted} beats ${computerChoiceConverted}`);
+//       humanScore += 1;
+//     } else if (humanChoiceConverted === "rock" && computerChoiceConverted === "paper"
+//       || humanChoiceConverted === "paper" && computerChoiceConverted === "scissors"
+//       || humanChoiceConverted === "scissors" && computerChoiceConverted === "rock"
+//     ) {
+//       console.log(`You lose... ${computerChoiceConverted} beats ${humanChoiceConverted}.`);
+//       computerScore += 1;
+//     } else {
+//       console.log(`It's a tie. You choose ${humanChoiceConverted} and Computer also!`);
+//     }
+//     console.log(`Votre score: ${humanScore} - Computer's score: ${computerScore}`);
+//   };
+// }
 
-playGame();
+// playGame();
+
+const body = document.querySelector("body");
+
+const btnRock = document.createElement("button");
+btnRock.classList.add("btn");
+btnRock.textContent = "Rock";
+
+const btnPaper = document.createElement("button");
+btnPaper.classList.add("btn");
+btnPaper.textContent = "Paper";
+
+const btnScissors = document.createElement("button");
+btnScissors.classList.add("btn");
+btnScissors.textContent = "Scissors";
+
+body.appendChild(btnRock);
+body.appendChild(btnPaper);
+body.appendChild(btnScissors);
